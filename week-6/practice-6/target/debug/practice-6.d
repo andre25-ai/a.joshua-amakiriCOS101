eb8e4345559658afc4bf4a38fce7d79ccbeb6743
unknown <andrew.joshua-amakiri@pau.edu.ng>
2025-11-05 17:27:52 +0100

    (My eight commit)

---
+++ week-6/practice-6/target/debug/practice-6.d
@@ -0,0 +1,1 @@
+C:\Users\User\OneDrive\Documents\a.joshua-amakiriCOS101\week-6\practice-6\target\debug\practice-6.exe: C:\Users\User\OneDrive\Documents\a.joshua-amakiriCOS101\week-6\practice-6\src\main.rs
