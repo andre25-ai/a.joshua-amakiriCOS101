39c31048cdbc09901bbee586163283e6e051add5
unknown <andrew.joshua-amakiri@pau.edu.ng>
2025-11-05 16:56:18 +0100

    (My ninth commit)

---
+++ week-6/practice-4/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+	let fullname= "Chibudum John Umeh";
+	let department="Computer Science";
+	let uni="Pan-Atantic University";
+
+	let mut school ="School of science".to_string();
+
+	school.push_str("and Technology");
+
+	println!("My name is:{}",fullname);
+
+	println!("The length of my fullname is:{}",fullname.len());
+    
+	println!("I am a student of {} Department",department);
+
+	println!("{}",school);
+	println!("{}",uni);
+}
