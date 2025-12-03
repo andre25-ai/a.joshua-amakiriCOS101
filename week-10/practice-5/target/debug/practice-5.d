6d9343413c7dca0d84d67b6c133328509be401ae
Obumneme-1 <obumneme.madubuko@pau.edu.ng>
2025-12-03 12:39:35 +0100

    Borrowing in rust

---
+++ week-10/practice-5/target/debug/practice-5.d
@@ -0,0 +1,1 @@
+C:\Users\User\OneDrive\Documents\a.joshua-amakiriCOS101\week-10\practice-5\target\debug\practice-5.exe: C:\Users\User\OneDrive\Documents\a.joshua-amakiriCOS101\week-10\practice-5\src\main.rs
